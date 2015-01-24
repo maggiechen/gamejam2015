6b9bfe40d073d14bc24408226ac7fec6d50471f0
willyao94 <williamyao1994@gmail.com>
2015-01-24 15:25:06 -0800

    Added door, door logic, and gamecontroller script

---
+++ sapandbran/Assets/Scripts/DoorController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			GameController.EndGame();
+		}
+	}
+}
---
+++ sapandbran/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void EndGame () {
+		//TODO: show menu
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public static void GameOver () {
+		//TODO: show menu
+		Application.Quit ();
+	}
+}
