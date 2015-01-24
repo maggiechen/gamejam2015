74740114c36432c87e5e9ae8e48aa5bd35de5b8d
Brandon Lim <b_loong5@hotmail.com>
2015-01-24 01:28:20 -0800

    Made the Player a prefab, froze Player rotation on all the axes so it doesn't roll when you move it. Added the IsJumpable tag to Platforms. Added jumping to the player script and it can only jump if it's currently touching a jumpable surface.
    - reviewed by ME AHAHAHA

--- sapandbran/Assets/Scripts/PlayerController.cs
+++ sapandbran/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	private float speed = 1;
+	public float speed = 1;
+
+    // TODO-BL Will change so we only change the jump magnitude in one place
+    public float jumpMagnitude = 100;
+    private bool canJump;
 	// Use this for initialization
 	void Start () {
-	
+        canJump = false;
 	}
 	
 	// Update is called once per frame
@@ -17,9 +21,41 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate() {
 		//Rigidbody rigidbody = new Rigidbody ();
 		float moveHorizontal = 10 * Input.GetAxis ("Horizontal");
-		float moveVertical = 10 * Input.GetAxis ("Vertical");
+        float moveVertical = 10 * Input.GetAxis("Vertical");
+        float jumpForce;
+
+        jumpForce = (Input.GetKeyDown(KeyCode.Space) && canJump) ? 100 : 0;
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            jumpForce = 100;
+            canJump = false;
+        }
+        else
+        {
+            jumpForce = 0;
+        }
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		
+        Vector3 jumpMovement = new Vector3 (0.0f, jumpForce, 0.0f);
+
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
+        rigidbody.AddForce (jumpMovement * jumpMagnitude * Time.deltaTime);
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Player collided with " + collision.gameObject.name);
+        if (collision.gameObject.tag == "IsJumpable")
+        {
+            canJump = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Player stopped touching " + collision.gameObject.name);
+        if (collision.gameObject.tag == "IsJumpable")
+        {
+            canJump = false;
+        }
+    }
 }
