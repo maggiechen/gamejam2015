c1794cf6482f75f1c0f025af8efcc8cb75fbab97
Vestige0214 <cpw4@sfu.ca>
2015-01-24 15:10:51 -0800

    Fixing movement. Implemented angled movement. Fixed camera position

--- sapandbran/Assets/Scripts/PlayerController.cs
+++ sapandbran/Assets/Scripts/PlayerController.cs
@@ -4,45 +4,43 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	public float speed = 1;
 
-    public float jumpMagnitude = 100;
+    // TODO-BL Will change so we only change the jump magnitude in one place
+    public float jumpMagnitude = 1000;
     private bool canJump;
 	// Use this for initialization
 	void Start () {
-        canJump = false;
+        canJump = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (isOutsideGameBoundaries())
-        {
-            // TODO the death trigger or whatever here
-            Debug.Log("The player is outside game boundaries");
-        }
+	
 	}
 
 	// called at fixed time intervals
 	void FixedUpdate() {
 		//Rigidbody rigidbody = new Rigidbody ();
-		float moveHorizontal = 1000 * Input.GetAxis ("Horizontal");
-        float moveVertical = 1000 * Input.GetAxis("Vertical");
-        float jumpForce;
-
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
-        {   
-            jumpForce = 100;
-            canJump = false;
-        }
-        else
-        {
-            jumpForce = 0;
-        }
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-        Vector3 jumpMovement = new Vector3 (0.0f, jumpForce, 0.0f);
-
+		float moveAxisX = 1000 * Input.GetAxisRaw ("Horizontal");
+        float moveAxisZ = 1000 * Input.GetAxisRaw("Vertical");
+        float jumpForce = 0;
+		Vector3 jumpMovement = new Vector3(0.0f, 0.0f, 0.0f);
+        if (Input.GetKeyDown (KeyCode.Space) && canJump) {
+			jumpForce = 100;
+			canJump = false;
+		}
+		jumpMovement.Set(0.0f, jumpForce, 0.0f);
+		Vector3 movement = VectorMovement (moveAxisX, moveAxisZ);
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
         rigidbody.AddForce (jumpMovement * jumpMagnitude * Time.deltaTime);
 	}
+	Vector3 VectorMovement(float moveAxisX, float moveAxisZ){
+		Vector3 moveX = new Vector3 (moveAxisX, 0.0f, 0.0f);
+		Vector3 moveZ = new Vector3 (0.0f, 0.0f, moveAxisZ);
+		Vector3 test = moveX + moveZ;
+		Debug.Log (test.x);
+		return moveX + moveZ;
 
+	}
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Player collided with " + collision.gameObject.name);
@@ -60,13 +58,4 @@ public class PlayerController : MonoBehaviour {
             canJump = false;
         }
     }
-
-    bool isOutsideGameBoundaries()
-    {
-		Vector3 position = transform.position;
-		int limit = 500;
-		return !((position.x > -limit && position.x < limit) &&
-			(position.y > -limit && position.y < limit) &&
-			(position.z > -limit && position.z < limit));
-	}
 }
