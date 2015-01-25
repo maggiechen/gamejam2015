5ff7b3a74a702a97de42fd9d57c94c19a53a0b6b
Maggie Chen <maggieyudanchen@gmail.com>
2015-01-24 16:24:45 -0800

    Added gravity switcher and ignored unnecessary files

--- sapandbran/Assets/Scripts/GravityChanger.cs
+++ sapandbran/Assets/Scripts/GravityChanger.cs
@@ -2,21 +2,60 @@
 using System.Collections;
 
 public class GravityChanger : MonoBehaviour {
+	public bool goThroughFloor;
+	public GameObject currentPlatform;
+	//public Vector3 moveDirection;
 
+	//private bool isMovingBack = false;
+	private Vector3 originalPosition;
 	// Use this for initialization
 	void Start () {
-	
+		originalPosition = currentPlatform.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		/*if (isMovingBack) {
+			Vector3 backVector = originalPosition - currentPlatform.transform.position;
+			backVector /= backVector.magnitude;
+			backVector *= moveDirection.magnitude;
+			currentPlatform.transform.Translate(backVector * Time.deltaTime);
+			if (backVector.magnitude < 1) {
+				isMovingBack = false;
+				Debug.Log ("goback");
+			}
+		}*/
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.rigidbody != null) {
-			//do stuff here
+			//other.rigidbody.velocity.x = 0;
+			//other.rigidbody.velocity.z = 0;
+
+			if (goThroughFloor) {
+				currentPlatform.SetActive(false);
+			} else {
+				changeGravity(other);
+			}
 		}
 	}
 
+/*	void OnTriggerStay(Collider other) {
+		if (goThroughFloor && other.gameObject != currentPlatform ) {
+			currentPlatform.transform.Translate(moveDirection * Time.deltaTime);
+		}
+	}*/
+
+	void OnTriggerExit(Collider other) {
+		if (goThroughFloor && other.gameObject != currentPlatform) {
+			Debug.Log ("Exited");
+			changeGravity (other);
+			currentPlatform.SetActive(true);
+			//isMovingBack = true;
+		}
+	}
+
+	void changeGravity(Collider other) {
+		other.gameObject.GetComponent<Gravity>().fallDirection = other.gameObject.GetComponent<Gravity>().fallDirection * -1;
+	}
 }
