344a90e3d1228f18340f82b97be8fda61cbc861e
Vestige0214 <cpw4@sfu.ca>
2015-01-25 14:29:05 -0800

    made the level easier and implemented dying after falling

--- sapandbran/Assets/Scripts/PlayerController.cs
+++ sapandbran/Assets/Scripts/PlayerController.cs
@@ -11,8 +11,12 @@ public class PlayerController : MonoBehaviour {
 	private float jumpMagnitude = 150;
 	private int friction = 3;
     private float distToGround;
+	private float fallHeightDeath = 20.0f;
     private bool isAntiGravityOn = false;
     private int reverseDirection = 1;
+
+    //private float deltaDY = 0.0f;
+    private double FALL_CONSTANT = -0.06147593;
 
 	// Use this for initialization
 	void Start () {
@@ -21,9 +25,7 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Mathf.Abs(rigidbody.velocity.y) > maxSpeedY) {
-			GameController.GameOver ();
-		}
+
 	}
 
 	// called at fixed time intervals
@@ -49,6 +51,7 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = VectorMovement (moveAxisX, moveAxisZ);
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
         rigidbody.AddForce (jumpMovement * jumpMagnitude * Time.deltaTime);
+		death ();
 	}
 
 	Vector3 VectorMovement(float moveAxisX, float moveAxisZ){
@@ -74,5 +77,10 @@ public class PlayerController : MonoBehaviour {
     }
 	public void rotatePlayer(){
 		transform.RotateAround (transform.position, transform.right, 180f);
+	}
+	void death(){
+		if (Mathf.Abs (transform.position.y) > fallHeightDeath) {
+			GameController.GameOver();
+		}
 	}
 }
