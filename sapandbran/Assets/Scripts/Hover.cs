a1bee5a4a692531f534e23beddc104138507c788
Maggie Chen <maggieyudanchen@gmail.com>
2015-01-24 19:12:54 -0800

    Made gravity changer hover and added particle systems to both sides

---
+++ sapandbran/Assets/Scripts/Hover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+	float index = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		index += 0.1f;
+		gameObject.transform.Translate (new Vector3 (0f, Mathf.Sin (index) / 200, 0f));
+	}
+}
