73f1c76bf3d383ed9b84b0a250a45339a46669a9
willyao94 <williamyao1994@gmail.com>
2015-01-25 01:47:29 -0800

    Fixed player moving at a slight angle

--- sapandbran/Assets/Scripts/PlayerController.cs
+++ sapandbran/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 1;
 
 	public static int maxSpeedY = 20;
+	public static int maxSpeed = 500;
+
+	private int inputMultiplier = 150;
 
     // TODO-BL Will change so we only change the jump magnitude in one place
     public float jumpMagnitude = 1000;
     private float distToGround;
 	private bool isAntiGravityOn = false;
+
 	// Use this for initialization
 	void Start () {
         distToGround = collider.bounds.extents.y;
@@ -25,8 +29,13 @@ public class PlayerController : MonoBehaviour {
 	// called at fixed time intervals
 	void FixedUpdate() {
 		//Rigidbody rigidbody = new Rigidbody ();
-		float moveAxisX = 1000 * Input.GetAxisRaw ("Horizontal");
-        float moveAxisZ = 1000 * Input.GetAxisRaw ("Vertical");
+		float moveAxisX = inputMultiplier * Input.GetAxisRaw ("Horizontal");
+		float moveAxisZ = inputMultiplier * Input.GetAxisRaw ("Vertical");
+
+		if (moveAxisX == 0 && moveAxisZ == 0 && rigidbody.velocity != Vector3.zero) {
+			rigidbody.drag = 1;
+		}
+
         float jumpForce = 0;
 		Vector3 jumpMovement = new Vector3(0.0f, 0.0f, 0.0f);
         if (Input.GetKeyDown(KeyCode.Space) && IsTouchingGround())
@@ -44,11 +53,7 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	Vector3 VectorMovement(float moveAxisX, float moveAxisZ){
-		Vector3 moveX = new Vector3 (moveAxisX, 0.0f, 0.0f);
-		Vector3 moveZ = new Vector3 (0.0f, 0.0f, moveAxisZ);
-		Vector3 test = moveX + moveZ;
-		Debug.Log (test.x);
-		return moveX + moveZ;
+		return new Vector3 (moveAxisX, 0.0f, 0.0f) + new Vector3 (0.0f, 0.0f, moveAxisZ);
 	}
 	public void antiGravityOn(){
 		if (isAntiGravityOn) 
