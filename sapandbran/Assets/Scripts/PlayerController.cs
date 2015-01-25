b82216e1293db991c0d96a1255b6ad3aa9ce6360
Brandon Lim <b_loong5@hotmail.com>
2015-01-24 18:41:48 -0800

    Fixed issue where jumping wasn't working when the player was up against a wall or other platform. Added skybox to camera to make the scene look a bit prettier.

--- sapandbran/Assets/Scripts/PlayerController.cs
+++ sapandbran/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,10 @@ public class PlayerController : MonoBehaviour {
 
     // TODO-BL Will change so we only change the jump magnitude in one place
     public float jumpMagnitude = 1000;
-    private bool canJump;
+    private float distToGround;
 	// Use this for initialization
 	void Start () {
-        canJump = true;
+        distToGround = collider.bounds.extents.y;
 	}
 	
 	// Update is called once per frame
@@ -24,38 +24,27 @@ public class PlayerController : MonoBehaviour {
         float moveAxisZ = 1000 * Input.GetAxisRaw("Vertical");
         float jumpForce = 0;
 		Vector3 jumpMovement = new Vector3(0.0f, 0.0f, 0.0f);
-        if (Input.GetKeyDown (KeyCode.Space) && canJump) {
+        if (Input.GetKeyDown(KeyCode.Space) && IsTouchingGround())
+        {
 			jumpForce = 100;
-			canJump = false;
 		}
 		jumpMovement.Set(0.0f, jumpForce, 0.0f);
 		Vector3 movement = VectorMovement (moveAxisX, moveAxisZ);
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
         rigidbody.AddForce (jumpMovement * jumpMagnitude * Time.deltaTime);
 	}
+
 	Vector3 VectorMovement(float moveAxisX, float moveAxisZ){
 		Vector3 moveX = new Vector3 (moveAxisX, 0.0f, 0.0f);
 		Vector3 moveZ = new Vector3 (0.0f, 0.0f, moveAxisZ);
 		Vector3 test = moveX + moveZ;
 		Debug.Log (test.x);
 		return moveX + moveZ;
-
 	}
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Player collided with " + collision.gameObject.name);
-        if (collision.gameObject.tag == "IsJumpable")
-        {
-            canJump = true;
-        }
-    }
 
-    void OnCollisionExit(Collision collision)
+    bool IsTouchingGround()
     {
-        Debug.Log("Player stopped touching " + collision.gameObject.name);
-        if (collision.gameObject.tag == "IsJumpable")
-        {
-            canJump = false;
-        }
+        Vector3 down = Vector3.down;
+        return (Physics.Raycast(transform.position, down, distToGround)) ? true : false;
     }
 }
